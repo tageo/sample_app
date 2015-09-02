01af6a17ad378cc7e51718ed5e59ab2de503683a
tageo <ttaaggeeoo@gmail.com>
2015-09-02 08:39:41 +0000

    ユーザーをフォローする

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -3,19 +3,19 @@ namespace :db do
   task populate: :environment do
     make_users
     make_microposts
-    #make_relationships
+    make_relationships
   end
 end
 
 def make_users
   admin = User.create!(name:     "Example User",
-                       email:    "example@railstutorial.org",
+                       email:    "example@railstutorial.jp",
                        password: "foobar",
                        password_confirmation: "foobar",
                        admin: true)
   99.times do |n|
     name  = Faker::Name.name
-    email = "example-#{n+1}@railstutorial.org"
+    email = "example-#{n+1}@railstutorial.jp"
     password  = "password"
     User.create!(name:     name,
                  email:    email,
@@ -30,4 +30,13 @@ def make_microposts
     content = Faker::Lorem.sentence(5)
     users.each { |user| user.microposts.create!(content: content) }
   end
+end
+
+def make_relationships
+  users = User.all
+  user  = users.first
+  followed_users = users[2..50]
+  followers      = users[3..40]
+  followed_users.each { |followed| user.follow!(followed) }
+  followers.each      { |follower| follower.follow!(user) }
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -53,20 +53,71 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 	end
 
 	describe "profile page" do
-	  let(:user) { FactoryGirl.create(:user) }
-	  let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
-	  let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
-	  # ユーザー変数を作成するためのコードに置き換える。
-	  before { visit user_path(user) }
+		let(:user) { FactoryGirl.create(:user) }
+		let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+		let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+		# ユーザー変数を作成するためのコードに置き換える。
+		before { visit user_path(user) }
 
-	  it { should have_content(user.name) }
-	  it { should have_title(user.name) }
+		it { should have_content(user.name) }
+		it { should have_title(user.name) }
 
-	  describe "microposts" do
-		it { should have_content(m1.content) }
-		it { should have_content(m2.content) }
-		it { should have_content(user.microposts.count) }
-	  end
+		describe "microposts" do
+			it { should have_content(m1.content) }
+			it { should have_content(m2.content) }
+			it { should have_content(user.microposts.count) }
+		end
+
+		describe "follow/unfollow buttons" do
+			let(:other_user) { FactoryGirl.create(:user) }
+			before { sign_in user }
+
+			describe "following a user" do
+				before { visit user_path(other_user) }
+
+				it "should increment the followed user count" do
+					expect do
+						click_button "Follow"
+					end.to change(user.followed_users, :count).by(1)
+				end
+
+				it "should increment the other user's followers count" do
+					expect do
+						click_button "Follow"
+					end.to change(other_user.followers, :count).by(1)
+				end
+
+				describe "toggling the button" do
+					before { click_button "Follow" }
+					it { should have_xpath("//input[@value='Unfollow']") }
+				end
+			end
+
+			describe "unfollowing a user" do
+				before do
+					user.follow!(other_user)
+					visit user_path(other_user)
+				end
+
+				it "should decrement the followed user count" do
+					expect do
+						click_button "Unfollow"
+					end.to change(user.followed_users, :count).by(-1)
+				end
+
+				it "should decrement the other user's followers count" do
+					expect do
+						click_button "Unfollow"
+					end.to change(other_user.followers, :count).by(-1)
+				end
+
+				describe "toggling the button" do
+					before { click_button "Unfollow" }
+					it { should have_xpath("//input[@value='Follow']") }
+				end
+			end
+		end
+
 
 	end
 
@@ -141,7 +192,33 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
 
 
+	describe "following/followers" do
+	    let(:user) { FactoryGirl.create(:user) }
+	    let(:other_user) { FactoryGirl.create(:user) }
+	    before { user.follow!(other_user) }
 
+	    describe "followed users" do
+	      before do
+	        sign_in user
+	        visit following_user_path(user)
+	      end
+
+	      it { should have_title('Following') }
+	      it { should have_selector('h3', text: 'Following') }
+	      it { should have_link(other_user.name, href: user_path(other_user)) }
+	    end
+
+	    describe "followers" do
+	      before do
+	        sign_in other_user
+	        visit followers_user_path(other_user)
+	      end
+
+	      it { should have_title('Followers') }
+	      it { should have_selector('h3', text: 'Followers') }
+	      it { should have_link(user.name, href: user_path(user)) }
+	    end
+	end
 
 
 
